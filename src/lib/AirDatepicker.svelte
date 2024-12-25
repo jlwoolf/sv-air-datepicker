528f02dd659e1dfbe24d16c9cadd54f36f7371e5
jonathan <jonathan@jlwoolf.com>
2024-12-25 15:12:17 -0700

    Created air-datepicker component

---
+++ src/lib/AirDatepicker.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import AirDatepicker, { type AirDatepickerOptions } from 'air-datepicker';
+	import localeEn from 'air-datepicker/locale/en';
+	import 'air-datepicker/air-datepicker.css';
+
+	interface AirDatePickerProps extends AirDatepickerOptions {
+		/** an html element to bind the picker to */
+		el?: HTMLElement;
+		class?: string;
+	}
+
+	let { el, class: classNames, ...opts }: AirDatePickerProps = $props();
+	let input = $state<HTMLInputElement>();
+	let picker: AirDatepicker | undefined;
+
+	$effect(() => {
+		if (picker) picker.destroy();
+
+		if (el) {
+			picker = new AirDatepicker(el as HTMLInputElement, {
+				locale: localeEn,
+				classes: classNames,
+				...opts
+			});
+		} else if (input) {
+			picker = new AirDatepicker(input, {
+				locale: localeEn,
+				classes: classNames,
+				...opts
+			});
+		}
+	});
+</script>
+
+{#if !el}
+	<input bind:this={input} />
+{/if}
+
+<style>
+</style>
