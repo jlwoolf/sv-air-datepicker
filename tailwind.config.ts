1a36edb40b2d9e4ce4330eae9001c047d218da10
jonathan <jonathan@jlwoolf.com>
2025-01-03 19:16:22 -0700

    Added daisyui to make sure it works with it's colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss';
 import adpPlugin from './src/lib/tailwindcss';
+import daisyui from 'daisyui';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -8,5 +9,5 @@ export default {
 		extend: {}
 	},
 
-	plugins: [adpPlugin]
+	plugins: [adpPlugin, daisyui]
 } satisfies Config;
